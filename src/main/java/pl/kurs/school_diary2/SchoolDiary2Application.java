29e1334643efcd497f93edee531eac969aee0954
mateusz.trochimowicz <trohimowicz@onet.pl>
2023-03-04 12:55:13 +0100

    initial commit

---
+++ src/main/java/pl/kurs/school_diary2/SchoolDiary2Application.java
@@ -0,0 +1,13 @@
+package pl.kurs.school_diary2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolDiary2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolDiary2Application.class, args);
+    }
+
+}
