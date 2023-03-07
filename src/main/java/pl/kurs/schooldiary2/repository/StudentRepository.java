844aca7b7599028e1dd3a2162903784b69f44344
mateusz.trochimowicz <trohimowicz@onet.pl>
2023-03-07 13:17:40 +0100

    changes in StudentRepository

--- src/main/java/pl/kurs/schooldiary2/repository/StudentRepository.java
+++ src/main/java/pl/kurs/schooldiary2/repository/StudentRepository.java
@@ -1,8 +1,9 @@
 package pl.kurs.schooldiary2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.kurs.schooldiary2.model.Student;
 
-public interface StudentRepository extends JpaRepository<StudentRepository, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
 
 }
